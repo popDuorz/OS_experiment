8fe0567e35e106de529ff9a25ddee167325f5b9d
popduorz <popduorz@gmail.com>
2018-06-13 09:58:37 +0800

    finished

--- OS_experiment/3/VirtualStorage.py
+++ OS_experiment/3/VirtualStorage.py
@@ -1,44 +1,103 @@
 import sys
+import random
+from time import sleep
+
+
 class VirtualStorage(object):
-    def __init__(self,num):
+    def __init__(self, num):
         self.page_num = num
         self.lost = 0
         self.hit = 0
         self.page = []
         for i in range(num):
             self.page.append(0)
+        hit = False
 
-    def fifo(self,page_id):
+    def fifo(self, page_id):
+        new = page_id
+        old = self.page[0]
+        changed_page_num = self.page_num
         if page_id in self.page:
             self.hit = self.hit + 1
+            hit = True
+            changed_page_num = self.page.index(page_id) + 1
         else:
+            hit = False
             for i in range(self.page_num - 1):
                 self.page[i] = self.page[i + 1]
             self.page[self.page_num - 1] = page_id
             self.lost = self.lost + 1
-            self.print_result()
+        self.print_result(old, new, changed_page_num, hit)
 
-    def lru(self,page_id):
+    def lru(self, page_id):
+        new = page_id
+        old = self.page[0]
+        changed_page_num = self.page_num
         if page_id in self.page:
+            hit = True
+            changed_page_num = self.page.index(page_id) + 1
             self.hit = self.hit + 1
             for i in range(self.page.index(page_id), self.page_num - 1):
                 self.page[i] = self.page[i + 1]
+
         else:
+            hit = False
             self.lost = self.lost + 1
             for i in range(self.page_num - 1):
                 self.page[i] = self.page[i + 1]
         self.page[self.page_num - 1] = page_id
-        self.print_result()
+        self.print_result(old, new, changed_page_num, hit)
 
-    def print_result(self):
+    def print_result(self, old, new, changed_page_num, hit):
+        if hit:
+            action = 'Hit in page ' + str(changed_page_num) + ' and refresh pages'
+        elif old == 0:
+            action = 'Lost and insert to page ' + str(changed_page_num)
+        else:
+            action = 'Replace ' + str(old) + ' -> ' + str(new) + ' to ' + 'page ' + str(changed_page_num)
         for i in range(self.page_num):
             sys.stdout.write(str(self.page[i]) + '   ')
-        sys.stdout.write(str(self.hit) + '   ' + str(self.lost) + '\n')
+        sys.stdout.write(' ' + str(self.hit) + '   ' + str(self.lost) + '    ' + action + '\n')
+        sleep(1)
+
 
 if __name__ == '__main__':
-    # content = [1,2,3,4,2,1,5,6,2,1,2,3,7,6,3,2,1,2,3,6]
-    content = [1, 2, 3, 4, 2, 1]
-    virtual_storage = VirtualStorage(4)
-    print('1   2   3   4  hit lost\n')
-    for i in range(len(content)):
-        virtual_storage.lru(content[i])
+    page_num = input('Input the page number:')
+    rw = input('Input the r/w times number:')
+    content = []
+    for r in range(int(rw)):
+        content.append(random.randint(1, int(page_num)+1))
+    virtual_storage = VirtualStorage(int(page_num))
+
+    print('Choose a approach:')
+    print('1.fifo')
+    print('2.lru')
+    choice = input()
+    print('The access sequence is: ', end='')
+    for c in content:
+        print(c, end=' ')
+    print()
+
+    if choice == '1':
+        for i in range(int(page_num)):
+            print('\033[1;21;42m'+str(i+1) + '   ' + '\033[0m', end='')
+        print('\033[1;21;41mhit lost    action\033[0m')
+        for i in range(int(page_num)):
+            print('0', end='   ')
+        print()
+        for i in range(len(content)):
+            virtual_storage.fifo(content[i])
+
+    elif choice == '2':
+        for i in range(int(page_num)):
+            print('\033[1;21;42m'+str(i+1) + '   ' + '\033[0m', end='')
+        print('\033[1;21;41mhit lost    action\033[0m')
+        for i in range(int(page_num)):
+            print('0', end='   ')
+        print()
+        for i in range(int(content)):
+            virtual_storage.lru(content[i])
+    hitRatio = float(virtual_storage.hit/int(rw)) * 100
+    print('The hit ratio is: %.1f%%' % hitRatio)
+    print()
+
--- OS_experiment/4/DiskManager.py
+++ OS_experiment/4/DiskManager.py
@@ -1,74 +1,93 @@
 import matplotlib.pyplot as plt 
+import random
 class Disk(object):
-    def __init__(self):
-        self.list = [98,183,37,122,14,124,65,69]
-        self.position = 100
-        self.move = 0
-        self.x = [100]
-        self.y = range(9)
+    def __init__(self, _start_position, _movement_num):
+        self.list_for_sstf = [] 
+        for m in range(_movement_num):
+            self.list_for_sstf.append(random.randint(1, 1000) % 500)
+        self.list_for_scan = self.list_for_sstf.copy()
+        self.position = _start_position
+        self.move_for_sstf = 0
+        self.move_for_scan = 0
+        self.x_for_sstf = [_start_position]
+        self.y_for_sstf = range(_movement_num + 1)
+        self.x_for_scan = [_start_position]
+        self.y_for_scan = range(_movement_num + 1)
 
     def sstf(self):
         pos = -1
-        while len(self.list) != 0:
-            for i in range(len(self.list)):
+        while len(self.list_for_sstf) != 0:
+            for i in range(len(self.list_for_sstf)):
                 a = 100000
-                if abs(self.list[i] - self.position) < a:
-                    a = abs(self.list[i] - self.position)
+                if abs(self.list_for_sstf[i] - self.position) < a:
+                    a = abs(self.list_for_sstf[i] - self.position)
                     pos = i
-            self.move = self.move + abs(self.list[pos] - self.position)
-            print(self.list[pos],"move length:",self.move)
-            self.x.append(self.list[pos])
-            self.position = self.list[pos]
-            self.list.pop(pos)
+            self.move_for_sstf = self.move_for_sstf + abs(self.list_for_sstf[pos] - self.position)
+            print(self.list_for_sstf[pos],"sstf move length:",self.move_for_sstf)
+            self.x_for_sstf.append(self.list_for_sstf[pos])
+            self.position = self.list_for_sstf[pos]
+            self.list_for_sstf.pop(pos)
 
     def scan(self, cur_dir):
         direction = cur_dir
-        while len(self.list) != 0:
+        while len(self.list_for_scan) != 0:
             if direction == 0:
                 a = 10000000
                 pos = -1
-                for i in range(len(self.list)):
-                    if (self.list[i] - self.position) < 0 and abs(self.list[i] - self.position) < a:
-                        a = abs(self.list[i] - self.position)
+                for i in range(len(self.list_for_scan)):
+                    if (self.list_for_scan[i] - self.position) < 0 and abs(self.list_for_scan[i] - self.position) < a:
+                        a = abs(self.list_for_scan[i] - self.position)
                         pos = i
                 if pos != -1:
-                    self.move = self.move + abs(self.list[pos] - self.position)
-                    print(self.list[pos],"move length",self.move)
-                    self.position = self.list[pos]
-                    self.x.append(self.list[pos])
-                    self.list.pop(pos)
+                    self.move_for_scan = self.move_for_scan + abs(self.list_for_scan[pos] - self.position)
+                    print(self.list_for_scan[pos],"move length",self.move_for_scan)
+                    self.position = self.list_for_scan[pos]
+                    self.x_for_scan.append(self.list_for_scan[pos])
+                    self.list_for_scan.pop(pos)
                 else:
                     direction = 1
             else:
                 a = 10000000
                 pos = -1
-                for i in range(len(self.list)):
-                    if (self.list[i] - self.position) > 0 and abs(self.list[i] - self.position) < a:
-                        a = abs(self.list[i] - self.position)
+                for i in range(len(self.list_for_scan)):
+                    if (self.list_for_scan[i] - self.position) > 0 and abs(self.list_for_scan[i] - self.position) < a:
+                        a = abs(self.list_for_scan[i] - self.position)
                         pos = i
                 if pos != -1:
-                    self.move = self.move + abs(self.list[pos] - self.position)
-                    print(self.list[pos],"move length",self.move)
-                    self.position = self.list[pos]
-                    self.x.append(self.list[pos])
-                    self.list.pop(pos)
+                    self.move_for_scan = self.move_for_scan + abs(self.list_for_scan[pos] - self.position)
+                    print(self.list_for_scan[pos],"move length",self.move_for_scan)
+                    self.position = self.list_for_scan[pos]
+                    self.x_for_scan.append(self.list_for_scan[pos])
+                    self.list_for_scan.pop(pos)
                 else:
                     direction = 0
 
 if __name__ == '__main__':
-    disk = Disk()
-    print("initial position",disk.position)
-    disk.scan(1)
+    _start_position = input('Input the start position:')
+    _movement_num = input('Input the movement number:')
+
+    disk = Disk(int(_start_position), int(_movement_num))
+    print("initial position", disk.position)
     plt.figure(figsize = (10,8))
     ax = plt.axes()
-    plt.plot(disk.x, disk.y, label='path', linewidth=0, color='white', marker='o', markerfacecolor='red',markersize='5')
-    for i in range(0,len(disk.x) - 1):
-        ax.annotate("", xy=(disk.x[i+1], disk.y[i+1]), xytext=(disk.x[i], disk.y[i]), arrowprops=dict(arrowstyle="->"))
+
+    disk.scan(1)
+    plt.plot(disk.x_for_scan, disk.y_for_scan, label='SCAN', linewidth=0, color='white', marker='o', markerfacecolor='red',markersize='10')
+    for i in range(0,len(disk.x_for_scan) - 1):
+        ax.annotate("", xy=(disk.x_for_scan[i+1], disk.y_for_scan[i+1]), xytext=(disk.x_for_scan[i], disk.y_for_scan[i]), arrowprops=dict(arrowstyle="->", facecolor='red'))
+
+    disk.sstf()
+    plt.plot(disk.x_for_sstf, disk.y_for_sstf, label='SSTF', linewidth=0, color='white', marker='v', markerfacecolor='blue',markersize='10')
+    for i in range(0,len(disk.x_for_sstf) - 1):
+        ax.annotate("", xy=(disk.x_for_sstf[i+1], disk.y_for_sstf[i+1]), xytext=(disk.x_for_sstf[i], disk.y_for_sstf[i]), arrowprops=dict(arrowstyle="->", facecolor='blue'))
     # plt.plot(disk.x,disk.y,label='path',linewidth=3,color='black', marker='o', markerfacecolor='red',markersize=12)
     plt.xlabel('position')
     plt.ylabel('No.')
     plt.legend()
     plt.show()
+
+
+
 
 
 
